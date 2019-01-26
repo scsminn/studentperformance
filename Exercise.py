f01ae2f3cbe4baa726fbb25174fa0ba223cb23f6
Gina2223 <46614858+Gina2223@users.noreply.github.com>
2019-01-26 10:02:46 +0800

    Add files via upload

---
+++ Exercise.py
@@ -0,0 +1,13 @@
+# Importing the Libraries 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset 
+dataset = pd.read_csv('grads.csv')
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'na', strategy = 'mean', axis = 0)
+imputer.fit[:, 3].values
